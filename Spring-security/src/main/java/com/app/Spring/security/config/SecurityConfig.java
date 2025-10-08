3a796ce170628a85e2caecaf40a293af8de7a302
Leandro HernandezSr <l.hernandez071999@gmail.com>
2025-10-07 23:52:55 -0300

    ADD: Se configura http basic para loguearse desde cURls

--- Spring-security/src/main/java/com/app/Spring/security/config/SecurityConfig.java
+++ Spring-security/src/main/java/com/app/Spring/security/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.app.Spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -18,28 +21,35 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                //.httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 // Cuales urls estaran protegidas y cuales no
                 .authorizeHttpRequests(auth -> {
                     // Endpoints publicos
                     auth.requestMatchers("/v1/index").permitAll();
                     // Endpoints privados
                     auth.requestMatchers("/v1/index2").authenticated();
-                    // Denegar los demas endpoints sin declarar
+
+                    auth.anyRequest().authenticated();
+
                 }).formLogin(form -> 
                     form.successHandler(successHandler())
                 ).sessionManagement(session->{
                     session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
                     session.invalidSessionUrl("/login");
-                    session.maximumSessions(1).expiredUrl("/login");
-                    session.sessionFixation().migrateSession();
+                    session.maximumSessions(1).expiredUrl("/login").sessionRegistry(sessionRegistry());
+                    session.sessionFixation().newSession();
                 })
                 .build();
     }
 
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+            }
+
     public AuthenticationSuccessHandler successHandler() {
         return (request, response, authentication) -> {
-            response.sendRedirect("/v1/index");
+            response.sendRedirect("/v1/session");
         };
     }
 
--- Spring-security/src/main/java/com/app/Spring/security/controllers/CustomerController.java
+++ Spring-security/src/main/java/com/app/Spring/security/controllers/CustomerController.java
@@ -1,14 +1,26 @@
 package com.app.Spring.security.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Controller
 @RequestMapping("/v1")
 public class CustomerController {
+
+    @Autowired
+    private SessionRegistry sessionRegistry;
 
     @GetMapping("/index")
     public ResponseEntity<String> index(){
@@ -18,5 +30,33 @@ public class CustomerController {
     @GetMapping("/index2")
     public ResponseEntity<String> index2(){
         return new ResponseEntity<>("Hello world NOT SECURED!",HttpStatus.OK);
+    }
+
+    @GetMapping("/session")
+    public ResponseEntity<?> getDetailsSession(){
+        AtomicReference<String> sessionId= new AtomicReference<>("");
+        AtomicReference<User> userObject= new AtomicReference<>();
+
+        List<Object> sessions=sessionRegistry.getAllPrincipals();
+
+        sessions.forEach(session -> {
+            if(session instanceof User){
+                userObject.set((User) session);
+            }
+
+            List<SessionInformation>sessionInformations=sessionRegistry.getAllSessions(userObject.get(),false);
+
+            sessionInformations.forEach(sessionInformation -> {
+               sessionId.set(sessionInformation.getSessionId());
+            });
+        });
+
+        Map<String,Object> map=new HashMap<>();
+
+        map.put("response","Hello world!");
+        map.put("sessionId",sessionId.get());
+        map.put("user",userObject.get());
+
+        return new  ResponseEntity<>(map,HttpStatus.OK);
     }
 }
