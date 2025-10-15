bf83af9f1c08bf5dd8a416bf718568b572706841
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-10-15 00:30:29 -0300

    Add: se comienza a probar la creacion de usuario

--- app.security/src/main/java/com/lhernandez/app/security/configs/SpringSecurityConfiguration.java
+++ app.security/src/main/java/com/lhernandez/app/security/configs/SpringSecurityConfiguration.java
@@ -2,10 +2,17 @@ package com.lhernandez.app.security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,10 +27,27 @@ public class SpringSecurityConfiguration {
         .httpBasic(Customizer.withDefaults())
         .authorizeHttpRequests(http->{
             http.requestMatchers("/api/hello-not-secured").permitAll();
+            http.requestMatchers("/api/user/create").permitAll();
             http.anyRequest().authenticated();
         });
 
         return https.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,PasswordEncoder passwordEncoder){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
 }
--- app.security/src/main/java/com/lhernandez/app/security/services/UserDetailsServiceImpl.java
+++ app.security/src/main/java/com/lhernandez/app/security/services/UserDetailsServiceImpl.java
@@ -1,17 +1,34 @@
 package com.lhernandez.app.security.services;
 
+import java.util.Optional;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.lhernandez.app.security.mappers.UserMapper;
+import com.lhernandez.app.security.models.entities.UserEntity;
+import com.lhernandez.app.security.repositories.UserRepository;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented me>thod 'loadUserByUsername'");
+        Optional<UserEntity>userEntityOpt=userRepository.findByUsername(username);
+        
+        if(userEntityOpt.isPresent()){
+            return UserMapper.toUserDetails(userEntityOpt.get());
+        }
+
+        return null;
     }
 
 }
