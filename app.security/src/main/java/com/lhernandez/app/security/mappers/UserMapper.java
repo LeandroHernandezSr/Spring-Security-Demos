534db717f1fb218be77b5bcc281298ab626aaa5f
Leandro HernandezSr <l.hernandez071999@gmail.com>
2025-10-14 22:55:41 -0300

    Add: se finaliza creacion del mapper

--- app.security/src/main/java/com/lhernandez/app/security/mappers/UserMapper.java
+++ app.security/src/main/java/com/lhernandez/app/security/mappers/UserMapper.java
@@ -2,30 +2,56 @@ package com.lhernandez.app.security.mappers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.lhernandez.app.security.models.dtos.UserDto;
+import com.lhernandez.app.security.models.entities.RoleEntity;
 import com.lhernandez.app.security.models.entities.UserEntity;
-
 
 public class UserMapper {
 
-    private UserMapper(){}
-    
-    public static UserDetails toUserDetails(UserEntity userEntity){
+    private UserMapper() {
+    }
 
-        List<SimpleGrantedAuthority>authorities=new ArrayList<>();
+    public static UserDetails toUserDetails(UserEntity userEntity) {
 
-        userEntity.getRoles().forEach(role->{
-            authorities.add(new SimpleGrantedAuthority("ROLE_"+role));
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        userEntity.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
         });
 
         return User.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+    
+    public static UserEntity toEntity(UserDto userDto) {
+        return new UserEntity.Builder()
+                .username(userDto.getUsername())
+                .email(userDto.getEmail())
+                .password(userDto.getPassword())
+                .roles(userDto.getRoles().stream().map(rol -> {
+                    RoleEntity roleEntity = new RoleEntity();
+                    roleEntity.setRole(rol);
+                    return roleEntity;
+                }).collect(Collectors.toSet()))
+                .build();
+    }
+
+
+    public static UserDto toDto(UserEntity userEntity){
+        return new UserDto.Builder()
             .username(userEntity.getUsername())
+            .email(userEntity.getEmail())
             .password(userEntity.getPassword())
-            .authorities(authorities)
+            .roles(userEntity.getRoles().stream().map(role->role.getRole()).collect(Collectors.toSet()))
             .build();
     }
 
